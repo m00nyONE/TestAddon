f7c49de5fbd8bcf94595e6c29afb4a3840d999d5
m00nyONE <severin.meier.x@gmail.com>
2024-11-26 00:52:01 +0100

    add workflows

--- TestAddon.lua
+++ TestAddon.lua
@@ -1,7 +1,16 @@
-TestAddon = TestAddon or {}
+TestAddon = {
+    name = "TestAddon",
+    version = "dev",
+}
+
 local TA = TestAddon
 
-TA.name = "TestAddon"
+local function _createSlashCommands()
+    SLASH_COMMANDS["/testaddon"] = function(args)
+        d(TA.name .." is running")
+        d("input: " .. args)
+    end
+end
 
 local function Initialize()
     d("test")
@@ -9,8 +18,9 @@ end
 
 -- register the addon
 EVENT_MANAGER:RegisterForEvent(TA.name, EVENT_ADD_ON_LOADED, function(_, name)
-    if name ~= LYM.name then return end
+    if name ~= TA.name then return end
     EVENT_MANAGER:UnregisterForEvent(TA.name, EVENT_ADD_ON_LOADED)
 
     Initialize()
+    _createSlashCommands()
 end)
